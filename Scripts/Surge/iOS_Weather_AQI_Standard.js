d2f467efb625fb83bacdf9990b314c1ce0b903e4
Hackl0us <hello@hackl0us.com>
2020-12-28 14:28:47 +0800

    添加对 Apple 原生地图 / Apple Watch 原生天气 的支持；支持显示和风天气没有覆盖到地区的 AQI

--- Scripts/Surge/iOS_Weather_AQI_Standard.js
+++ Scripts/Surge/iOS_Weather_AQI_Standard.js
@@ -6,7 +6,7 @@ const aqicnToken = ''
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
-	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include=, requires-body=true, script-path=/path/to/iOS_Weather_AQI_Standard.js
+	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?, requires-body=true, script-path=/path/to/iOS_Weather_AQI_Standard.js
 
 	[MITM]
 	hostname = weather-data.apple.com
@@ -26,7 +26,7 @@ const AirQualityLevel = {
 	HAZARDOUS: 6
 }
 
-const coordRegex = /https:\/\/weather-data\.apple\.com\/v1\/weather\/[\w-]+\/([0-9]+\.[0-9]+)\/([0-9]+\.[0-9]+)\?include=/
+const coordRegex = /https:\/\/weather-data\.apple\.com\/v1\/weather\/[\w-]+\/([0-9]+\.[0-9]+)\/([0-9]+\.[0-9]+)\?/
 const [_, lat, lng] = $request.url.match(coordRegex)
 
 function classifyAirQualityLevel(aqiIndex) {
@@ -48,25 +48,72 @@ function classifyAirQualityLevel(aqiIndex) {
 function modifyWeatherResp(weatherRespBody, aqicnRespBody) {
 	let weatherRespJson = JSON.parse(weatherRespBody)
 	let aqicnRespJson = JSON.parse(aqicnRespBody).data
-
-	let aqicnIndex = aqicnRespJson.aqi
-	weatherRespJson.air_quality.airQualityCategoryIndex = classifyAirQualityLevel(aqicnIndex)
-	weatherRespJson.air_quality.airQualityIndex = aqicnIndex
-	weatherRespJson.air_quality.airQualityScale = AirQualityStandard.US
-	weatherRespJson.air_quality.learnMoreURL = aqicnRespJson.city.url + '/cn'
-
-	weatherRespJson.air_quality.metadata.provider_logo = 'https://i.loli.net/2020/12/27/UqW23eZLFAIbxGV.png'
-	weatherRespJson.air_quality.metadata.provider_name = 'aqicn.org'
-
-	weatherRespJson.air_quality.primaryPollutant = aqicnRespJson.dominentpol
-	weatherRespJson.air_quality.source = aqicnRespJson.city.name
-
+	weatherRespJson.air_quality = constructAirQuailityNode(aqicnRespJson)
 	return JSON.stringify(weatherRespJson)
+}
+
+function getPrimaryPollutant(pollutant) {
+	switch (pollutant) {
+		case 'co':
+			return 'CO2';
+		case 'so2':
+			return 'SO2';
+		case 'no2':
+			return 'NO2';
+		case 'pm25':
+			return 'PM2.5';
+		case 'pm10':
+			return 'PM10';
+		case 'o3':
+			return 'OZONE';
+		default:
+			return "OTHER";
+	}
+}
+
+function constructAirQuailityNode(aqicnData) {
+	let airQualityNode = { "source": "", "learnMoreURL": "", "isSignificant": true, "airQualityCategoryIndex": 1, "airQualityScale": "", "airQualityIndex": 0, "pollutants": { "CO": { "name": "CO", "amount": 0, "unit": "μg/m3" }, "SO2": { "name": "SO2", "amount": 0, "unit": "μg/m3" }, "NO2": { "name": "NO2", "amount": 0, "unit": "μg/m3" }, "PM2.5": { "name": "PM2.5", "amount": 0, "unit": "μg/m3" }, "OZONE": { "name": "OZONE", "amount": 0, "unit": "μg/m3" }, "PM10": { "name": "PM10", "amount": 0, "unit": "μg/m3" } }, "metadata": { "reported_time": 0, "longitude": 0, "provider_name": "aqicn.org", "expire_time": 2, "provider_logo": "https://i.loli.net/2020/12/27/UqW23eZLFAIbxGV.png", "read_time": 2, "latitude": 0, "v": 1, "language": "", "data_source": 0 }, "name": "AirQuality", "primaryPollutant": "" }
+	const aqicnIndex = aqicnData.aqi
+	airQualityNode.source = aqicnData.city.name
+	airQualityNode.learnMoreURL = aqicnData.city.url + '/cn'
+	airQualityNode.airQualityCategoryIndex = classifyAirQualityLevel(aqicnIndex)
+	airQualityNode.airQualityScale = AirQualityStandard.US
+	airQualityNode.airQualityIndex = aqicnIndex
+	airQualityNode.pollutants.CO.amount = aqicnData.iaqi.co.v
+	airQualityNode.pollutants.SO2.amount = aqicnData.iaqi.so2.v
+	airQualityNode.pollutants.NO2.amount = aqicnData.iaqi.no2.v
+	airQualityNode.pollutants["PM2.5"].amount = aqicnData.iaqi.pm25.v
+	airQualityNode.pollutants.OZONE.amount = aqicnData.iaqi.o3.v
+	airQualityNode.pollutants.PM10.amount = aqicnData.iaqi.pm10.v
+	airQualityNode.metadata.latitude = aqicnData.city.geo[0]
+	airQualityNode.metadata.longitude = aqicnData.city.geo[1]
+	airQualityNode.metadata.read_time = roundHours(new Date(), 'down')
+	airQualityNode.metadata.expire_time = roundHours(new Date(), 'up')
+	airQualityNode.metadata.reported_time = aqicnData.time.v
+	//airQualityNode.metadata.language = $request.headers['Accept-Language']
+	airQualityNode.primaryPollutant = getPrimaryPollutant(aqicnData.dominentpol)
+	return airQualityNode
+}
+
+function roundHours(time, method) {
+	switch (method) {
+		case 'up':
+			time.setHours(time.getHours() + Math.ceil(time.getMinutes() / 60));
+			break;
+		case 'down':
+			time.setHours(time.getHours() + Math.floor(time.getMinutes() / 60));
+			break;
+		default:
+			console.log("Error rounding method");
+	}
+	time.setMinutes(2, 0, 0);
+	return time;
 }
 
 $httpClient.get(`https://api.waqi.info/feed/geo:${lat};${lng}/?token=${aqicnToken}`, function (error, _response, data) {
 	if (error) {
-		$done({})
+		let body = $response.body
+		$done({ body })
 	} else {
 		let body = modifyWeatherResp($response.body, data)
 		$done({ body })
