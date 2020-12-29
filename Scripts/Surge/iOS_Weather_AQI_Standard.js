4aef743d1baf321f2308248d3f64336960c6f68d
刘国梁 <70141664+liu135800@users.noreply.github.com>
2020-12-30 00:25:55 +0800

    Update iOS_Weather_AQI_Standard.js

--- Scripts/Surge/iOS_Weather_AQI_Standard.js
+++ Scripts/Surge/iOS_Weather_AQI_Standard.js
@@ -1,7 +1,7 @@
 // Developed by Hackl0us (https://github.com/hackl0us)
 
 // STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
-const aqicnToken = ''
+const aqicnToken = 'https://api.waqi.info/feed/beijing/?token=bb53dc7602eb8fa1909cf634041c43106c3e9174'
 
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
