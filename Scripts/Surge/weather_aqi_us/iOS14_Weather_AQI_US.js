4744122010912f71a5eeb5418efe156ba8d65e4d
Hackl0us <hello@hackl0us.com>
2021-06-15 23:57:48 +0800

    更新 iOS 14 天气脚本，修正在 Apple Watch 上数据不正常显示的问题

--- Scripts/Surge/weather_aqi_us/iOS14_Weather_AQI_US.js
+++ Scripts/Surge/weather_aqi_us/iOS14_Weather_AQI_US.js
@@ -1,3 +1,4 @@
+// 此脚本仅适用于 iOS 14 的天气 App
 // Developed by Hackl0us (https://github.com/hackl0us)
 
 // STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
@@ -6,15 +7,15 @@ const aqicnToken = ''
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
-	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?, requires-body=true, script-path=/path/to/iOS_Weather_AQI_Standard.js
+	iOS14美标空气质量 = type=http-response,pattern=https://weather-data.apple.com/v1/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/iOS14_Weather_AQI_US.js
 
 	[MITM]
 	hostname = weather-data.apple.com
 */
 
 const AirQualityStandard = {
-	CN: 'HJ6332012.1',
-	US: 'EPA_NowCast.1'
+	CN: 'HJ6332012.4',
+	US: 'EPA_NowCast.4'
 }
 
 const AirQualityLevel = {
@@ -26,8 +27,10 @@ const AirQualityLevel = {
 	HAZARDOUS: 6
 }
 
-const coordRegex = /https:\/\/weather-data\.apple\.com\/v1\/weather\/[\w-]+\/([0-9]+\.[0-9]+)\/([0-9]+\.[0-9]+)\?/
-const [_, lat, lng] = $request.url.match(coordRegex)
+const MillisecondsConversion = 1000
+const coordRegex = /https:\/\/weather-data\.apple\.com\/v1\/weather\/([\w-]+)\/(-?[0-9]+\.[0-9]+)\/(-?[0-9]+\.[0-9]+)\?/
+const [_, language, lat, lng] = $request.url.match(coordRegex)
+
 
 function classifyAirQualityLevel(aqiIndex) {
 	if (aqiIndex >= 0 && aqiIndex <= 50) {
@@ -67,48 +70,53 @@ function getPrimaryPollutant(pollutant) {
 		case 'o3':
 			return 'OZONE';
 		default:
-			console.log('Unknown pollutant ' + pollutant);
+			return "OTHER";
 	}
 }
 
 function constructAirQuailityNode(aqicnData) {
-	let airQualityNode = { "source": "", "learnMoreURL": "", "isSignificant": true, "airQualityCategoryIndex": 1, "airQualityScale": "", "airQualityIndex": 0, "pollutants": { "CO": { "name": "CO", "amount": 0, "unit": "μg/m3" }, "SO2": { "name": "SO2", "amount": 0, "unit": "μg/m3" }, "NO2": { "name": "NO2", "amount": 0, "unit": "μg/m3" }, "PM2.5": { "name": "PM2.5", "amount": 0, "unit": "μg/m3" }, "OZONE": { "name": "OZONE", "amount": 0, "unit": "μg/m3" }, "PM10": { "name": "PM10", "amount": 0, "unit": "μg/m3" } }, "metadata": { "reported_time": 0, "longitude": 0, "provider_name": "aqicn.org", "expire_time": 2, "provider_logo": "https://i.loli.net/2020/12/27/UqW23eZLFAIbxGV.png", "read_time": 2, "latitude": 0, "v": 1, "language": "", "data_source": 0 }, "name": "AirQuality", "primaryPollutant": "" }
+	let airQualityNode = { "source": "", "learnMoreURL": "", "isSignificant": true, "airQualityCategoryIndex": 1, "airQualityScale": "", "airQualityIndex": 0, "pollutants": { "CO": { "name": "CO", "amount": 0, "unit": "μg/m3" }, "SO2": { "name": "SO2", "amount": 0, "unit": "μg/m3" }, "NO2": { "name": "NO2", "amount": 0, "unit": "μg/m3" }, "PM2.5": { "name": "PM2.5", "amount": 0, "unit": "μg/m3" }, "OZONE": { "name": "OZONE", "amount": 0, "unit": "μg/m3" }, "PM10": { "name": "PM10", "amount": 0, "unit": "μg/m3" } }, "metadata": { "reported_time": 0, "longitude": 0, "provider_name": "aqicn.org", "expire_time": 2, "provider_logo": "https://aqicn.org/mapi/logo.png", "read_time": 2, "latitude": 0, "version": 1, "language": "", "data_source": 0 }, "name": "AirQuality", "primaryPollutant": "" }
 	const aqicnIndex = aqicnData.aqi
 	airQualityNode.source = aqicnData.city.name
 	airQualityNode.learnMoreURL = aqicnData.city.url + '/cn/m'
+
 	airQualityNode.airQualityCategoryIndex = classifyAirQualityLevel(aqicnIndex)
 	airQualityNode.airQualityScale = AirQualityStandard.US
 	airQualityNode.airQualityIndex = aqicnIndex
+	airQualityNode.primaryPollutant = getPrimaryPollutant(aqicnData.dominentpol)
+
 	airQualityNode.pollutants.CO.amount = aqicnData.iaqi.co?.v || -1
 	airQualityNode.pollutants.SO2.amount = aqicnData.iaqi.so2?.v || -1
 	airQualityNode.pollutants.NO2.amount = aqicnData.iaqi.no2?.v || -1
 	airQualityNode.pollutants["PM2.5"].amount = aqicnData.iaqi.pm25?.v || -1
 	airQualityNode.pollutants.OZONE.amount = aqicnData.iaqi.o3?.v || -1
 	airQualityNode.pollutants.PM10.amount = aqicnData.iaqi.pm10?.v || -1
+
 	airQualityNode.metadata.latitude = aqicnData.city.geo[0]
 	airQualityNode.metadata.longitude = aqicnData.city.geo[1]
-	airQualityNode.metadata.read_time = roundHours(new Date(), 'down')
-	airQualityNode.metadata.expire_time = roundHours(new Date(), 'up')
-	airQualityNode.metadata.reported_time = aqicnData.time.v
-	//airQualityNode.metadata.language = $request.headers['Accept-Language']
-	airQualityNode.primaryPollutant = getPrimaryPollutant(aqicnData.dominentpol)
+	airQualityNode.metadata.reported_time = timeConversion(new Date(aqicnData.time.iso), 'remain')
+	airQualityNode.metadata.read_time = timeConversion(new Date(), 'remain')
+	airQualityNode.metadata.expire_time = timeConversion(new Date(aqicnData.time.iso), 'add-1h-floor')
+	airQualityNode.metadata.language = language
+
 	return airQualityNode
 }
 
-function roundHours(time, method) {
-	switch (method) {
-		case 'up':
-			time.setHours(time.getHours() + Math.ceil(time.getMinutes() / 60));
+function timeConversion(time, action) {
+	switch (action) {
+		case 'remain':
+			time.setMilliseconds(0);
 			break;
-		case 'down':
-			time.setHours(time.getHours() + Math.floor(time.getMinutes() / 60));
+		case 'add-1h-floor':
+			time.setHours(time.getHours() + 1);
+			time.setMinutes(0, 0, 0);
 			break;
 		default:
-			console.log("Error rounding method");
+			console.log("Error time converting action.");
 	}
-	time.setMinutes(2, 0, 0);
-	return time;
+	return time.getTime() / MillisecondsConversion;
 }
+
 
 $httpClient.get(`https://api.waqi.info/feed/geo:${lat};${lng}/?token=${aqicnToken}`, function (error, _response, data) {
 	if (error) {
