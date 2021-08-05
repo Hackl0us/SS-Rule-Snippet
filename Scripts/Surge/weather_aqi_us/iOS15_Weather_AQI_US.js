30c374c6d70f4cdcd6029279879977add49e18f4
seventan <75936766+chloelxxlxx@users.noreply.github.com>
2021-08-05 15:54:11 +0800

    Update iOS15_Weather_AQI_US.js

--- Scripts/Surge/weather_aqi_us/iOS15_Weather_AQI_US.js
+++ Scripts/Surge/weather_aqi_us/iOS15_Weather_AQI_US.js
@@ -2,7 +2,7 @@
 // Developed by Hackl0us (https://github.com/hackl0us)
 
 // STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
-const aqicnToken = ''
+const aqicnToken = 'd1024c9bc6504f0b58a29f6f1d989e2bece45cd7'
 
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
