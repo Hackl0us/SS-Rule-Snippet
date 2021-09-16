cc3f27e59ec678de3528268d1be94df2375b281a
Hackl0us <hello@hackl0us.com>
2021-09-16 15:11:35 +0800

    修正天气脚本对 iOS 15 正式版的支持

--- Scripts/Surge/weather_aqi_us/iOS14_Weather_AQI_US.js
+++ Scripts/Surge/weather_aqi_us/iOS14_Weather_AQI_US.js
@@ -14,8 +14,8 @@ const aqicnToken = ''
 */
 
 const AirQualityStandard = {
-	CN: 'HJ6332012.4',
-	US: 'EPA_NowCast.4'
+	CN: 'HJ6332012.2111',
+	US: 'EPA_NowCast.2111'
 }
 
 const AirQualityLevel = {
--- Scripts/Surge/weather_aqi_us/iOS15_Weather_AQI_US.js
+++ Scripts/Surge/weather_aqi_us/iOS15_Weather_AQI_US.js
@@ -7,15 +7,15 @@ const aqicnToken = ''
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
-	iOS15美标空气质量 = type=http-response,pattern=https://weather-data.apple.com/v2/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/iOS15_Weather_AQI_US.js
+	iOS15美标空气质量 = type=http-response,pattern=https://weather-data.apple.com/v2/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,max-size=0,script-path=path/to/iOS15_Weather_AQI_US.js
 
 	[MITM]
 	hostname = weather-data.apple.com
 */
 
 const AirQualityStandard = {
-	CN: 'HJ6332012.4',
-	US: 'EPA_NowCast.4'
+	CN: 'HJ6332012.2111',
+	US: 'EPA_NowCast.2111'
 }
 
 const AirQualityLevel = {
